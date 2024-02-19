c09769b622ca11fb20271bc00be476603d06a022
GultacB <160045911+GultacB@users.noreply.github.com>
2024-02-19 14:08:44 +0400

    task4

---
+++ task4.2/task4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Vaxti daxil edin:");
+string time = Console.ReadLine();
+switch (time)
+{
+    case "8 saat 5 deqiqe ":
+       Console.WriteLine("sabahiniz xeyr!");
+        break;
+
+    case "16 saat 27 deqiqe":
+        Console.WriteLine("gunortaniz xeyr!");
+        break;
+    case "22 saat 45 deqiqe":
+        Console.WriteLine("axsaminiz xeyr!");
+        break;
+}
---
+++ task4.3/task4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("oyun kartinin seriya nomresini daxil edin:");
+int number=int.Parse(Console.ReadLine());
+switch (number)
+{
+    case 000:
+        Console.WriteLine("snake0");
+        break;
+    case 001:
+        Console.WriteLine("spider1");
+        break;
+    case 002:
+        Console.WriteLine("turtle2");
+        break;
+    case 003:
+        Console.WriteLine("whale3");
+        break;
+    case 004:
+        Console.WriteLine("wolf4");
+        break;
+    case 005:
+        Console.WriteLine("zebra5");
+        break;   
+}
